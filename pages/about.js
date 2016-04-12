aff7f69d07d531f78bb944beb83095e6d7aec1d0
Tiago Rodrigues <tmcrodrigues@gmail.com>
2016-04-12 10:12:08 +0200

    about form styling

--- pages/about.js
+++ pages/about.js
@@ -4,17 +4,17 @@ PC.pages.about = {}
 
 PC.pages.about.renderHTML = function (params) {
   return Promise.resolve(
-    '<div>' +
+    '<form id="aboutForm" class="settings-form">' +
       '<p><a href="https://contentful.com">Contentful</a> is a content management platform for web applications, mobile apps and connected devices.</p>'+
       '<p>It allows you to create, edit & manage content in the cloud and publish it anywhere via a powerful API.</p>'+
       '<p>This Product Catalogue demo app uses the Content Model of our Product Space Template.</p>'+
       '<p>If you want to try it with your own space, please access the <a href="https://app.contentful.com">Contentful Web App</a>, create a new space, load it with the Product Template and keep the same “Content Model”.'+
-      '<form id="aboutForm">'+
+      '<div class="form-container">'+
         '<p>Space ID: <input id="spaceId" type="text" /></p>'+
         '<p>Access Token: <input id="accessToken" type="text" /></p>'+
         '<p><button>Load Space</button></p>'+
-      '</form>'+
-    '</div>'
+      '</div>'+
+    '</form>'
   )
 }
 
